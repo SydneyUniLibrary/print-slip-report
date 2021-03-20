39ef0355e0f491c339982b698b073382c97c5273
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-03-21 10:07:15 +1100

    Add a code to ColumnDefinition.

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -11,6 +11,7 @@ import { escape } from 'html-escaper'
 class ColumnDefinition {
 
   constructor(
+    public code: string,
     public name: string,
     public mapFn: (requestedResource: any) => string
   ) {}
@@ -19,22 +20,22 @@ class ColumnDefinition {
 
 
 const COLUMNS_DEFINITIONS = [
-  new ColumnDefinition('Title', x => x?.resource_metadata?.title),
-  new ColumnDefinition('Location', x => x?.location?.shelving_location),
-  new ColumnDefinition('Call Number', x => x?.location?.call_number),
-  new ColumnDefinition('Author', x => x?.resource_metadata?.author),
-  new ColumnDefinition('ISBN', x => x?.resource_metadata?.isbn),
-  new ColumnDefinition('ISSN', x => x?.resource_metadata?.issn),
-  new ColumnDefinition('Publisher', x => x?.resource_metadata?.publisher),
-  new ColumnDefinition('Publication Date', x => x?.resource_metadata?.publication_year),
-  new ColumnDefinition('Request Type', x => x?.request?.[0]?.request_sub_type?.desc),
-  new ColumnDefinition('Requested For', x => x?.request?.[0]?.requester?.desc),
-  new ColumnDefinition('Request ID', x => x?.request?.[0]?.id),
-  new ColumnDefinition('Barcode', x => x?.location?.copy?.[0]?.barcode),
-  new ColumnDefinition('Pickup Location', x => x?.request?.[0]?.destination?.desc),
-  new ColumnDefinition('Item Call Number', x => x?.location?.copy?.[0]?.alternative_call_number),
-  new ColumnDefinition('Request Note', x => x?.request?.[0]?.comment),
-  new ColumnDefinition('Storage Location ID', x => x?.location?.copy?.[0]?.storage_location_id),
+  new ColumnDefinition('title', 'Title', x => x?.resource_metadata?.title),
+  new ColumnDefinition('location','Location', x => x?.location?.shelving_location),
+  new ColumnDefinition('call-number', 'Call Number', x => x?.location?.call_number),
+  new ColumnDefinition('author', 'Author', x => x?.resource_metadata?.author),
+  new ColumnDefinition('isbn', 'ISBN', x => x?.resource_metadata?.isbn),
+  new ColumnDefinition('issn', 'ISSN', x => x?.resource_metadata?.issn),
+  new ColumnDefinition('publisher', 'Publisher', x => x?.resource_metadata?.publisher),
+  new ColumnDefinition('publication-date', 'Publication Date', x => x?.resource_metadata?.publication_year),
+  new ColumnDefinition('request-type', 'Request Type', x => x?.request?.[0]?.request_sub_type?.desc),
+  new ColumnDefinition('requested-for', 'Requested For', x => x?.request?.[0]?.requester?.desc),
+  new ColumnDefinition('request-id', 'Request ID', x => x?.request?.[0]?.id),
+  new ColumnDefinition('barcode', 'Barcode', x => x?.location?.copy?.[0]?.barcode),
+  new ColumnDefinition('pickup-location', 'Pickup Location', x => x?.request?.[0]?.destination?.desc),
+  new ColumnDefinition('item-call-number', 'Item Call Number', x => x?.location?.copy?.[0]?.alternative_call_number),
+  new ColumnDefinition('request-note', 'Request Note', x => x?.request?.[0]?.comment),
+  new ColumnDefinition('storage-location-id', 'Storage Location ID', x => x?.location?.copy?.[0]?.storage_location_id),
 ]
 
 
