d04aca7010d60313810d8f972c64756640ac2be8
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-03-21 14:18:53 +1100

    Add column checkboxes to the config page.

--- cloudapp/src/app/config/config.component.ts
+++ cloudapp/src/app/config/config.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { FormArray, FormBuilder } from '@angular/forms'
+import { COLUMNS_DEFINITIONS } from '../column-definitions'
+
+
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
-  styleUrls: ['./config.component.scss']
+  styleUrls: [ './config.component.scss' ],
 })
 export class ConfigComponent implements OnInit {
 
+  columnDefinitions = COLUMNS_DEFINITIONS
   saving = false
 
-  constructor() { }
+  form = this.formBuilder.group({
+    columns: this.formBuilder.array(
+      this.columnDefinitions.map(() => this.formBuilder.control(false)),
+    ),
+  })
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+
+  get columns(): FormArray {
+    return this.form.get('columns') as FormArray
+  }
+
 
   ngOnInit(): void {
   }
