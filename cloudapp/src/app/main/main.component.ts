8458763c4ecd093acb872efc45979222fe806f19
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-03-21 13:51:16 +1100

    Move the column definitions into its own file.

--- cloudapp/src/app/main/main.component.ts
+++ cloudapp/src/app/main/main.component.ts
@@ -6,37 +6,7 @@ import { CloudAppRestService, CloudAppEventsService, Request, HttpMethod,
 import { MatRadioChange } from '@angular/material/radio';
 import { FormArray, FormBuilder, FormControl, ValidationErrors, Validators } from '@angular/forms'
 import { escape } from 'html-escaper'
-
-
-class ColumnDefinition {
-
-  constructor(
-    public code: string,
-    public name: string,
-    public mapFn: (requestedResource: any) => string
-  ) {}
-
-}
-
-
-const COLUMNS_DEFINITIONS = [
-  new ColumnDefinition('title', 'Title', x => x?.resource_metadata?.title),
-  new ColumnDefinition('location','Location', x => x?.location?.shelving_location),
-  new ColumnDefinition('call-number', 'Call Number', x => x?.location?.call_number),
-  new ColumnDefinition('author', 'Author', x => x?.resource_metadata?.author),
-  new ColumnDefinition('isbn', 'ISBN', x => x?.resource_metadata?.isbn),
-  new ColumnDefinition('issn', 'ISSN', x => x?.resource_metadata?.issn),
-  new ColumnDefinition('publisher', 'Publisher', x => x?.resource_metadata?.publisher),
-  new ColumnDefinition('publication-date', 'Publication Date', x => x?.resource_metadata?.publication_year),
-  new ColumnDefinition('request-type', 'Request Type', x => x?.request?.[0]?.request_sub_type?.desc),
-  new ColumnDefinition('requested-for', 'Requested For', x => x?.request?.[0]?.requester?.desc),
-  new ColumnDefinition('request-id', 'Request ID', x => x?.request?.[0]?.id),
-  new ColumnDefinition('barcode', 'Barcode', x => x?.location?.copy?.[0]?.barcode),
-  new ColumnDefinition('pickup-location', 'Pickup Location', x => x?.request?.[0]?.destination?.desc),
-  new ColumnDefinition('item-call-number', 'Item Call Number', x => x?.location?.copy?.[0]?.alternative_call_number),
-  new ColumnDefinition('request-note', 'Request Note', x => x?.request?.[0]?.comment),
-  new ColumnDefinition('storage-location-id', 'Storage Location ID', x => x?.location?.copy?.[0]?.storage_location_id),
-]
+import { ColumnDefinition, COLUMNS_DEFINITIONS } from '../column-definitions'
 
 
 @Component({
