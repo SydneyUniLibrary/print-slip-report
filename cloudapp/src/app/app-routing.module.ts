6a3a0c56cc2eefeac770c633ee7d98e3fe803b82
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-03-28 21:00:55 +1100

    Overhaul the popup window.
    * The popup window now mounts an Angular route.
    * The code for generating the HTML report has been replaced with an Angular template.
    * The responsibility for fetching the requests from Alma has moved into the popup window.
    * There is extensive cross-browser code for auto-printing and automatically closing the popup once the printing has happened.

--- cloudapp/src/app/app-routing.module.ts
+++ cloudapp/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { ConfigComponent } from './config/config.component'
 import { MainComponent } from './main/main.component'
+import { PrintSlipReportComponent } from './print-slip-report/print-slip-report.component'
 
 
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'config', component: ConfigComponent },
+  { path: 'print-slip-report', component: PrintSlipReportComponent }
 ]
 
 
