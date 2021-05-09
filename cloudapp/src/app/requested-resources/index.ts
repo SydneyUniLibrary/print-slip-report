7c2ab4208c23505ba7202ba666461d958664e675
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-05-09 17:51:39 +1000

    Rename requested-resources-service.ts to requested-resources.service.ts.

--- cloudapp/src/app/requested-resources/index.ts
+++ cloudapp/src/app/requested-resources/index.ts
@@ -1,3 +1,3 @@
 export { ColumnDefinition, COLUMNS_DEFINITIONS } from './column-definitions'
 export * from './requested-resources'
-export { InvalidParameterError, RequestedResourcesService } from './requested-resources-service'
+export { InvalidParameterError, RequestedResourcesService } from './requested-resources.service'
