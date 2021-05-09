6ddf40194192f581124c487c9531e4daf4dae9f9
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-05-09 16:56:16 +1000

    Add StringWithAttr type to requested-resources.ts.

--- cloudapp/src/app/requested-resources/index.ts
+++ cloudapp/src/app/requested-resources/index.ts
@@ -1,9 +1,3 @@
 export { ColumnDefinition, COLUMNS_DEFINITIONS } from './column-definitions'
-export {
-  RequestedResource,
-  RequestedResourceCopy,
-  RequestedResourceLocation,
-  RequestedResourceRequest,
-  RequestedResourceResourceMetadata,
-} from './requested-resources'
+export * from './requested-resources'
 export { InvalidParameterError, RequestedResourcesService } from './requested-resources-service'
--- cloudapp/src/app/requested-resources/requested-resources.ts
+++ cloudapp/src/app/requested-resources/requested-resources.ts
@@ -1,39 +1,52 @@
+export interface StringWithAttr {
+  desc: string
+  value: string
+}
+
+
+//---------------------------------------------------------------------------
+
+
 export interface RequestedResource {
   resource_metadata: RequestedResourceResourceMetadata
   location: RequestedResourceLocation
   request: RequestedResourceRequest[]
 }
 
+
 export interface RequestedResourceCopy {
   link: string
   pid: string
   barcode: string
-  base_status: { desc: string, value: string }
+  base_status: StringWithAttr
   alternative_call_number: string
   storage_location_id: string
 }
 
+
 export interface RequestedResourceLocation {
   holding_id: { link: string, value: string }
-  library: { desc: string, value: string }
+  library: StringWithAttr
   call_number: string
   shelving_location: string
   copy: RequestedResourceCopy[]
 }
 
+
 export interface RequestedResourceRequest {
   link: string
   id: string
   request_type: string
-  request_sub_type: { desc: string, value: string }
-  destination: { desc: string, value: string }
+  request_sub_type: StringWithAttr
+  destination: StringWithAttr
   request_date: string
   request_time: string
-  requester: { desc: string, value: string }
+  requester: StringWithAttr
   comment: string
   printed: boolean
   reported: boolean
 }
+
 
 export interface RequestedResourceResourceMetadata {
   mms_id: { link: string, value: string }
