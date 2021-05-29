05950133be1bf99c960103b8a41ba60fc8ce06d9
Jim Nicholls <jim.nicholls@sydney.edu.au>
2021-05-29 22:19:31 +1000

    Match requests to copies.

--- cloudapp/src/app/requested-resources/request-enrichment-service.ts
+++ cloudapp/src/app/requested-resources/request-enrichment-service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core'
-import { CloudAppRestService, HttpMethod } from '@exlibris/exl-cloudapp-angular-lib'
+import { CloudAppRestService } from '@exlibris/exl-cloudapp-angular-lib'
 import { LendingRequestsService } from './lending-requests-service'
-import { RequestedResource, RequestEnrichedRequestedResource } from './requested-resources'
+import { RequestedResource, RequestedResourceRequest, RequestEnrichedRequestedResource } from './requested-resources'
 
 
 
@@ -15,46 +15,93 @@ export class RequestEnrichmentService {
     private restService: CloudAppRestService,
   ) { }
 
+
   enrich(requestedResource: RequestedResource): Promise<void>[] | undefined {
+    if (requestedResource.request.length == 1) {
+      return this._enrichOneRequest(requestedResource)
+    } else {
+      return this._enrichManyRequests(requestedResource)
+    }
+  }
+
+
+  private _enrichOneRequest(requestedResource: RequestedResource): Promise<void>[] | undefined {
     return (
-      requestedResource?.request?.map(async r => {
-        let enrichedRequest = (r as RequestEnrichedRequestedResource['request'][number])
-        let almaRequest = r.link ? await this._fetchRequest(r.link) : undefined
+      requestedResource.request.map(async req => {
+        let almaRequest = req.link ? await this._fetchTitleRequest(req.link) : undefined
         if (almaRequest) {
-          enrichedRequest.request_sub_type = almaRequest.request_sub_type
-          enrichedRequest.volume = almaRequest.volume
-          enrichedRequest.issue = almaRequest.issue
-          enrichedRequest.chapter_or_article_title = almaRequest.chapter_or_article_title
-          enrichedRequest.chapter_or_article_author = almaRequest.chapter_or_article_author
-          if (almaRequest.required_pages_range) {
-            enrichedRequest.required_pages_range = almaRequest.required_pages_range
-          }
-          if (almaRequest.resource_sharing) {
-            enrichedRequest.resource_sharing = almaRequest.resource_sharing
-            if (almaRequest?.request_sub_type?.value?.startsWith('RESOURCE_SHARING')) {
-              try {
-                let lendingRequestsForLibrary = (
-                  await this.lendingRequestsService.lendingRequestsForLibraryWithStatus(
-                    almaRequest.pickup_location_library,
-                    enrichedRequest.resource_sharing.status.value,
-                  )
-                )
-                let lendingRequest = lendingRequestsForLibrary.find(
-                  r => r.request_id == enrichedRequest.resource_sharing.id
-                )
-                enrichedRequest.resource_sharing.volume = lendingRequest.volume
-              } catch (e) {
-                console.error(e)
-              }
-            }
+          await this._enrichRequest(req, almaRequest)
+        }
+        req.copies = new Set(requestedResource.location.copy)
+        console.log('RequestEnrichmentService _enrichOneRequest', req)
+      })
+    )
+  }
+
+
+  private _enrichManyRequests(requestedResource: RequestedResource): Promise<void>[] | undefined {
+    let reqMap = new Map<string, RequestedResource['request'][number]>(
+      requestedResource.request.map(r => [ r.id, r ])
+    )
+    return (
+      requestedResource.location.copy.map(async copy => {
+        console.log('RequestEnrichmentService _enrichManyRequests copy', copy)
+        for (let almaRequest of copy.link ? await this._fetchItemRequests(`${ copy.link }/requests`) : []) {
+          let req = reqMap.get(almaRequest.request_id)
+          if (req.copies?.size) {
+            req.copies.add(copy)
+            console.log('RequestEnrichmentService _enrichManyRequests add', req)
+          } else {
+            await this._enrichRequest(req, almaRequest)
+            req.copies = new Set([ copy ])
+            console.log('RequestEnrichmentService _enrichManyRequests enrich', req)
           }
         }
       })
     )
   }
 
-  private async _fetchRequest(requestUrl: string): Promise<any> {
-    return this.restService.call({ url: requestUrl, method: HttpMethod.GET }).toPromise()
+
+  private async _enrichRequest(request: RequestedResourceRequest, almaRequest) {
+    let enrichedRequest = (request as RequestEnrichedRequestedResource['request'][number])
+    enrichedRequest.request_sub_type = almaRequest.request_sub_type
+    enrichedRequest.volume = almaRequest.volume
+    enrichedRequest.issue = almaRequest.issue
+    enrichedRequest.chapter_or_article_title = almaRequest.chapter_or_article_title
+    enrichedRequest.chapter_or_article_author = almaRequest.chapter_or_article_author
+    if (almaRequest.required_pages_range) {
+      enrichedRequest.required_pages_range = almaRequest.required_pages_range
+    }
+    if (almaRequest.resource_sharing) {
+      enrichedRequest.resource_sharing = almaRequest.resource_sharing
+      if (almaRequest?.request_sub_type?.value?.startsWith('RESOURCE_SHARING')) {
+        try {
+          let lendingRequestsForLibrary = (
+            await this.lendingRequestsService.lendingRequestsForLibraryWithStatus(
+              almaRequest.pickup_location_library,
+              enrichedRequest.resource_sharing.status.value,
+            )
+          )
+          let lendingRequest = lendingRequestsForLibrary.find(
+            r => r.request_id == enrichedRequest.resource_sharing.id,
+          )
+          enrichedRequest.resource_sharing.volume = lendingRequest.volume
+        } catch (e) {
+          console.error(e)
+        }
+      }
+    }
+  }
+
+
+  private async _fetchItemRequests(url: string): Promise<any> {
+    let resp = await this.restService.call({ url: url }).toPromise()
+    return resp.user_request
+  }
+
+
+  private _fetchTitleRequest(url: string): Promise<any> {
+    return this.restService.call({ url: url }).toPromise()
   }
 
 }
